f8e33ad0a1ded5536c8c76fde7eab26419eb8c67
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-11 12:04:03 -0800

    Add Styling and shared header/footer files

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,7 +3,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string Description { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
